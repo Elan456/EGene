c77b1e03bdd830903c2b38401f5cb25f0af4f7d5
Owen <owen.anderson2018@gmail.com>
2022-05-30 12:52:51 -0400

    thing

--- PyGene/test.py
+++ PyGene/test.py
@@ -3,13 +3,17 @@ import pygame
 
 gameDisplay = pygame.display.set_mode((500, 500))
 
-ins = [[1,2,4,8], [2,4,8,16], [3,6,12,24]]
-#outs = [[1,2,3,4,5,6,7,8,9,18], [2,3,4,5,6,7,8,9,10,20]]
+ins = [[3],[5],[2]]
+outs = [[7],[11],[5]]  # output is 2x+1 and biased nodes have to be used
 
+guide = pygene.Species([1,1], 1, train_inputs=ins, train_outputs=outs, use_sigmoid=False, popsize=32, add_bias_nodes=True)
+for _ in range(100):  # if you break up the training to one at a time you can see the network change over time.
+    guide.train(1)
+    gameDisplay.blit(guide.get_best_agent().draw(gameDisplay), (0, 0))
+    pygame.display.update()
 
-guide = pygene.Species([4,1,2,4], 1, train_inputs=ins, train_outputs=ins, use_sigmoid=False, popsize=32, add_bias_nodes=True)
-guide.train(20)
-gameDisplay.blit(guide.get_best_agent().draw(gameDisplay), (0, 0))
-pygame.display.update()
-print(guide.get_best_agent().calico([4,8,16,32], show_internals=False))
-a = input()
+while True:
+    testins = []
+    for needed_input in range(len(ins[0])):
+        testins.append(float(input("Input" + str(needed_input) +":\n")))
+    print(guide.get_best_agent().calico(testins, show_internals=False))
