5d3bf57002331d084b5b4e63cabef46026a5d987
Ethan Laptop <telan4892@gmail.com>
2022-05-30 23:37:26 -0400

    Docs started. Deprecated species parameters removed

--- PyGene/test.py
+++ PyGene/test.py
@@ -3,17 +3,17 @@ import pygame
 
 gameDisplay = pygame.display.set_mode((500, 500))
 
-testset = "x3"
+testset = "xor"
 
 ins = {"xor": [[0, 0], [0, 1], [1, 0], [1, 1]],
        "x3": [[2],[4],[3],[1]]}[testset]
 outs = {"xor": [[0], [1], [1], [0]],
         "x3": [[6],[12],[9],[3]]}[testset]
 
-guide = pygene.Species([1,2,1], 1, train_inputs=ins, train_outputs=outs, use_sigmoid=True, popsize=1000, add_bias_nodes=True)
+guide = pygene.Species([2,2,1], 1, train_inputs=ins, train_outputs=outs, use_sigmoid=True, popsize=1000, add_bias_nodes=True)
 for _ in range(100):  # if you break up the training to one at a time you can see the network change over time.
     guide.train(1)
-    gameDisplay.blit(guide.get_best_agent().draw(gameDisplay), (0, 0))
+    gameDisplay.blit(guide.get_best_agent().draw(), (0, 0))
     pygame.display.update()
 
 while True:
