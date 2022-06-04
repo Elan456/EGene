e59734f9f85decdf2c03e574b3802412fb660493
EthanDesktop <telan4892@gmail.com>
2022-06-04 12:26:52 -0400

    Added icon for pygame window

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="EGene",
-    version="0.1.1",
+    version="0.1.2",
     author="Ethan Anderson",
     author_email="telan4892@gmail.com",
     url="https://github.com/Elan456/EGene",
